2806d9223854dfe17272bcf3eef922fdb0c32f39
driesva <Dries.VanAutreve@telenet.be>
2019-04-09 14:34:19 +0200

    APOCOW-5 vl-ui-textarea - release version voor css

--- vl-textarea.src.js
+++ vl-textarea.src.js
@@ -8,11 +8,11 @@ import { NativeVlElement } from '/node_modules/vl-ui-core/vl-core.js';
  * ### Attributen
  * Attribuut | Uitleg | Waarde
  * ----------|--------|--------
- * `block` | Wordt gebruikt om ervoor te zorgen dat de textarea getoond wordt als een block element en bijgevolg de breedte van de parent zal aannemen. | { boolean }
+ * `block` | Wordt gebruikt om ervoor te zorgen dat de textarea getoond wordt als een block element en bijgevolg de breedte van de parent zal aannemen. |
  * `error` | Wordt gebruikt om aan te duiden dat de textarea verplicht is of ongeldige tekst bevat. | { boolean }
  * `success` | Wordt gebruikt om aan te duiden dat de textarea correct werd ingevuld. | { boolean }
  * `disabled` | Wordt gebruikt om te voorkomen dat de gebruiker tekst in de textarea kan ingeven. | { boolean }
- * `focus` | Wordt gebruikt om de textarea . | { boolean }
+ * `focus` | Wordt gebruikt om de textarea focus te geven. | { boolean }
  *
  * @demo demo/vl-ui-textarea.html
  */
