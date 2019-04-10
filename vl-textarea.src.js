35370d7517a27eca25da2fb730807e8fc53c723f
Tom Coemans <tom.coemans@gmail.com>
2019-04-10 13:32:27 +0200

    WEBC-4 documentatie

--- vl-textarea.src.js
+++ vl-textarea.src.js
@@ -1,20 +1,17 @@
 import { NativeVlElement } from '/node_modules/vl-ui-core/vl-core.js';
 
 /**
- * vl-ui-textarea
- *
- * De vl-ui-textarea definieert een rechthoekig invoervak in een formulier, waarin de gebruiker over meerdere regels tekst kan invoeren.
- *
- * ### Attributen
- * Attribuut | Uitleg | Waarde
- * ----------|--------|--------
- * `block` | Wordt gebruikt om ervoor te zorgen dat de textarea getoond wordt als een block element en bijgevolg de breedte van de parent zal aannemen. |
- * `error` | Wordt gebruikt om aan te duiden dat de textarea verplicht is of ongeldige tekst bevat. | { boolean }
- * `success` | Wordt gebruikt om aan te duiden dat de textarea correct werd ingevuld. | { boolean }
- * `disabled` | Wordt gebruikt om te voorkomen dat de gebruiker tekst in de textarea kan ingeven. | { boolean }
- * `focus` | Wordt gebruikt om de textarea focus te geven. | { boolean }
- *
- * @demo demo/vl-ui-textarea.html
+ * VlTextArea
+ * @class
+ * @classdesc De vl-ui-textarea definieert een rechthoekig invoervak in een formulier, waarin de gebruiker over meerdere regels tekst kan invoeren. <a href="demo/vl-textarea.html">Demo</a>.
+ * 
+ * @extends VlElement
+ * 
+ * @property {boolean} block - Attribuut wordt gebruikt om ervoor te zorgen dat de textarea getoond wordt als een block element en bijgevolg de breedte van de parent zal aannemen.
+ * @property {boolean} error - Attribuut wordt gebruikt om aan te duiden dat de textarea verplicht is of ongeldige tekst bevat.
+ * @property {boolean} success - Attribuut wordt gebruikt om aan te duiden dat de textarea correct werd ingevuld. | { boolean }
+ * @property {boolean} disabled - Attribuut wordt gebruikt om te voorkomen dat de gebruiker tekst in de textarea kan ingeven. | { boolean }
+ * @property {boolean} focus - Attribuut wordt gebruikt om de textarea focus te geven.
  */
 export class VlTextarea extends NativeVlElement(HTMLTextAreaElement) {
 
