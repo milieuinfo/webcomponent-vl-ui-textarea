2853eab40c9b210f04aa930d86db035bf94e0fa9
Pascal Riquier <pascal.riquier@vlaanderen.be>
2020-02-25 16:35:52 +0100

    UIG-544 nieuwe projectstructuur en build scripts

--- src/vl-textarea.js
+++ src/vl-textarea.js
@@ -1,4 +1,4 @@
-import { NativeVlElement, define } from '/node_modules/vl-ui-core/vl-core.js';
+import { NativeVlElement, define } from '/node_modules/vl-ui-core/dist/vl-core.js';
 
 /**
  * VlTextArea
--- test/e2e/textarea.test.js
+++ test/e2e/textarea.test.js
@@ -9,7 +9,7 @@ describe('vl-textarea', async () => {
         return vlTextareaPage.load();
     });
 
-    after(async () => { 
-        return driver.quit();
+    it("Dummy test om de browsers te sluiten", () => {
+    	assert.isTrue(true);
     });
 });
