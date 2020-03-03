a8988065a650578e06ee726075446e359dc0530f
pascalriquier <pascalriquier@hotmail.com>
2020-03-03 13:34:12 +0100

    Introductie E2E testen (#45)

--- test/e2e/components/vl-textarea.js
+++ test/e2e/components/vl-textarea.js
@@ -1,10 +1,35 @@
 const { VlElement } = require('vl-ui-core').Test;
 
 class VlTextarea extends VlElement {  
-    constructor(driver, selector) {
-        super(driver, selector);
+
+    async setValue(text) {
+        await this.clear();
+        return this.sendKeys(text);
     }
 
+    async getValue() {
+    	return this.getAttribute("value");
+    }
+    
+    async isBlock() {
+    	return this.hasAttribute("block");
+    }
+
+    async isError() {
+    	return this.hasAttribute("error");
+    }
+
+    async isSuccess() {
+    	return this.hasAttribute("success");
+    }
+    
+    async isDisabled() {
+    	return this.hasAttribute("disabled");
+    }
+
+    async isFocus() {
+    	return this.hasAttribute("focus");
+    }
 }
 
 module.exports = VlTextarea;
--- test/e2e/pages/vl-textarea.page.js
+++ test/e2e/pages/vl-textarea.page.js
@@ -5,7 +5,31 @@ class VlTextareaPage extends Page {
     async _getTextarea(selector) {
         return new VlTextarea(this.driver, selector);
     }
+    
+    async getTextarea() {
+    	return this._getTextarea("#textarea");
+    }
 
+    async getTextareaBlock() {
+    	return this._getTextarea("#textarea-block");
+    }
+
+    async getTextareaError() {
+    	return this._getTextarea("#textarea-error");
+    }
+
+    async getTextareaSuccess() {
+    	return this._getTextarea("#textarea-success");
+    }
+    
+    async getTextareaDisabled() {
+    	return this._getTextarea("#textarea-disabled");
+    }
+    
+    async getTextareaFocus() {
+    	return this._getTextarea("#textarea-focus");
+    }
+    
     async load() {
         await super.load(Config.baseUrl + '/demo/vl-textarea.html');
     }
