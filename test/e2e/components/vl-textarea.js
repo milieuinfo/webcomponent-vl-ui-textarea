c89d97051ec90f36ee9fc1613ac6b593f7635b14
Tom Coemans <tom.coemans@gmail.com>
2020-06-03 16:54:18 +0200

    ESlint

--- test/e2e/components/vl-textarea.js
+++ test/e2e/components/vl-textarea.js
@@ -1,35 +1,34 @@
-const { VlElement } = require('vl-ui-core').Test;
+const {VlElement} = require('vl-ui-core').Test;
 
-class VlTextarea extends VlElement {  
+class VlTextarea extends VlElement {
+  async setValue(text) {
+    await this.clear();
+    return this.sendKeys(text);
+  }
 
-    async setValue(text) {
-        await this.clear();
-        return this.sendKeys(text);
-    }
+  async getValue() {
+    return this.getAttribute('value');
+  }
 
-    async getValue() {
-    	return this.getAttribute("value");
-    }
-    
-    async isBlock() {
-    	return this.hasAttribute("block");
-    }
+  async isBlock() {
+    return this.hasAttribute('block');
+  }
 
-    async isError() {
-    	return this.hasAttribute("error");
-    }
+  async isError() {
+    return this.hasAttribute('error');
+  }
 
-    async isSuccess() {
-    	return this.hasAttribute("success");
-    }
-    
-    async isDisabled() {
-    	return this.hasAttribute("disabled");
-    }
+  async isSuccess() {
+    return this.hasAttribute('success');
+  }
 
-    async isFocus() {
-    	return this.hasAttribute("focus");
-    }
+  async isDisabled() {
+    return this.hasAttribute('disabled');
+  }
+
+  async isFocus() {
+    return this.hasAttribute('focus');
+  }
 }
 
 module.exports = VlTextarea;
--- test/e2e/pages/vl-textarea.page.js
+++ test/e2e/pages/vl-textarea.page.js
@@ -1,38 +1,38 @@
 const VlTextarea = require('../components/vl-textarea');
-const { Page, Config } = require('vl-ui-core').Test;
+const {Page, Config} = require('vl-ui-core').Test;
 
 class VlTextareaPage extends Page {
-    async _getTextarea(selector) {
-        return new VlTextarea(this.driver, selector);
-    }
-    
-    async getTextarea() {
-    	return this._getTextarea("#textarea");
-    }
+  async _getTextarea(selector) {
+    return new VlTextarea(this.driver, selector);
+  }
 
-    async getTextareaBlock() {
-    	return this._getTextarea("#textarea-block");
-    }
+  async getTextarea() {
+    return this._getTextarea('#textarea');
+  }
 
-    async getTextareaError() {
-    	return this._getTextarea("#textarea-error");
-    }
+  async getTextareaBlock() {
+    return this._getTextarea('#textarea-block');
+  }
 
-    async getTextareaSuccess() {
-    	return this._getTextarea("#textarea-success");
-    }
-    
-    async getTextareaDisabled() {
-    	return this._getTextarea("#textarea-disabled");
-    }
-    
-    async getTextareaFocus() {
-    	return this._getTextarea("#textarea-focus");
-    }
-    
-    async load() {
-        await super.load(Config.baseUrl + '/demo/vl-textarea.html');
-    }
+  async getTextareaError() {
+    return this._getTextarea('#textarea-error');
+  }
+
+  async getTextareaSuccess() {
+    return this._getTextarea('#textarea-success');
+  }
+
+  async getTextareaDisabled() {
+    return this._getTextarea('#textarea-disabled');
+  }
+
+  async getTextareaFocus() {
+    return this._getTextarea('#textarea-focus');
+  }
+
+  async load() {
+    await super.load(Config.baseUrl + '/demo/vl-textarea.html');
+  }
 }
 
 module.exports = VlTextareaPage;
