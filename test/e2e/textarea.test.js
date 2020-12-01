f1f2f4a35a6602841a296f854b756a8780e2284f
Tom Coemans <tom.coemans@gmail.com>
2020-12-01 16:03:37 +0100

    browserstack

--- test/e2e/textarea.test.js
+++ test/e2e/textarea.test.js
@@ -1,16 +1,17 @@
 const {Config} = require('vl-ui-core').Test;
-const {assert, driver, By, Key} = require('vl-ui-core').Test.Setup;
+const {assert, getDriver, By, Key} = require('vl-ui-core').Test.Setup;
 const VlTextareaPage = require('./pages/vl-textarea.page');
 const {VlInputField} = require('vl-ui-input-field').Test;
 
 describe('vl-textarea', async () => {
-  const vlTextareaPage = new VlTextareaPage(driver);
+  let vlTextareaPage;
 
   beforeEach(() => {
+    vlTextareaPage = new VlTextareaPage(getDriver());
     return vlTextareaPage.load();
   });
 
-  it('Als gebruiker kan ik de tekst van een textarea invullen en lezen en leegmaken', async () => {
+  it('als gebruiker kan ik de tekst van een textarea invullen en lezen en leegmaken', async () => {
     const textarea = await vlTextareaPage.getTextarea();
     await textarea.setValue('Dit is de tekst in de textarea');
     await assert.eventually.equal(textarea.getValue(), 'Dit is de tekst in de textarea');
@@ -18,42 +19,42 @@ describe('vl-textarea', async () => {
     await assert.eventually.equal(textarea.getValue(), '');
   });
 
-  it('Als gebruiker kan ik het onderscheid zien tussen een inline textarea en een block textarea', async () => {
+  it('als gebruiker kan ik het onderscheid zien tussen een inline textarea en een block textarea', async () => {
     const textarea = await vlTextareaPage.getTextarea();
     await assert.eventually.isFalse(textarea.isBlock());
     const textareaBlock = await vlTextareaPage.getTextareaBlock();
     await assert.eventually.isTrue(textareaBlock.isBlock());
   });
 
-  it('Als gebruiker kan ik het onderscheid zien tussen een gewone textarea en een error textarea', async () => {
+  it('als gebruiker kan ik het onderscheid zien tussen een gewone textarea en een error textarea', async () => {
     const textarea = await vlTextareaPage.getTextarea();
     await assert.eventually.isFalse(textarea.isError());
     const textareaError = await vlTextareaPage.getTextareaError();
     await assert.eventually.isTrue(textareaError.isError());
   });
 
-  it('Als gebruiker kan ik het onderscheid zien tussen een gewone textarea en een success textarea', async () => {
+  it('als gebruiker kan ik het onderscheid zien tussen een gewone textarea en een success textarea', async () => {
     const textarea = await vlTextareaPage.getTextarea();
     await assert.eventually.isFalse(textarea.isSuccess());
     const textareaSuccess = await vlTextareaPage.getTextareaSuccess();
     await assert.eventually.isTrue(textareaSuccess.isSuccess());
   });
 
-  it('Als gebruiker kan ik het onderscheid zien tussen een gewone textarea en een focus textarea', async () => {
+  it('als gebruiker kan ik het onderscheid zien tussen een gewone textarea en een focus textarea', async () => {
     const textarea = await vlTextareaPage.getTextarea();
     await assert.eventually.isFalse(textarea.isFocus());
     const textareaFocus = await vlTextareaPage.getTextareaFocus();
     await assert.eventually.isTrue(textareaFocus.isFocus());
   });
 
-  it('Als gebruiker kan ik het onderscheid zien tussen een gewone textarea en een disabled textarea', async () => {
+  it('als gebruiker kan ik het onderscheid zien tussen een gewone textarea en een disabled textarea', async () => {
     const textarea = await vlTextareaPage.getTextarea();
     await assert.eventually.isFalse(textarea.isDisabled());
     const textareaDisabled = await vlTextareaPage.getTextareaDisabled();
     await assert.eventually.isTrue(textareaDisabled.isDisabled());
   });
 
-  it('Als gebruiker kan ik enkel een waarde zetten in een niet-disabled textarea', async () => {
+  it('als gebruiker kan ik enkel een waarde zetten in een niet-disabled textarea', async () => {
     const textarea = await vlTextareaPage.getTextarea();
     await assert.eventually.isTrue(textarea.isEnabled());
     const textareaDisabled = await vlTextareaPage.getTextareaDisabled();
@@ -66,14 +67,14 @@ describe('vl-textarea', async () => {
     }
   });
 
-  it('Als gebruiker kan ik het onderscheid zien tussen een geowne textarea en een rich textarea', async () => {
+  it('als gebruiker kan ik het onderscheid zien tussen een geowne textarea en een rich textarea', async () => {
     const textarea = await vlTextareaPage.getTextarea();
     await assert.eventually.isFalse(textarea.isRich());
     const textareaRich = await vlTextareaPage.getTextareaRich();
     await assert.eventually.isTrue(textareaRich.isRich());
   });
 
-  it('Als gebruiker kan ik tekst toevoegen en zal er automatisch een root paragraaf tag toegevoegd worden', async () => {
+  it('als gebruiker kan ik tekst toevoegen en zal er automatisch een root paragraaf tag toegevoegd worden', async () => {
     const textarea = await vlTextareaPage.getTextareaRich();
     await textarea.clear();
     const text = 'text';
@@ -81,7 +82,7 @@ describe('vl-textarea', async () => {
     await assert.eventually.include(textarea.getValue(), `<p>${text}</p>`);
   });
 
-  it('Als gebruiker kan ik mijn tekst van bold, italic, underline en strikethrough stijl voorzien', async () => {
+  it('als gebruiker kan ik mijn tekst van bold, italic, underline en strikethrough stijl voorzien', async () => {
     if (Config.browserName != 'chrome') {
       const textarea = await vlTextareaPage.getTextareaRich();
       await textarea.clear();
@@ -107,7 +108,7 @@ describe('vl-textarea', async () => {
     }
   });
 
-  it('Als gebruiker kan ik titels toevoegen', async () => {
+  it('als gebruiker kan ik titels toevoegen', async () => {
     const textarea = await vlTextareaPage.getTextareaRich();
     await textarea.clear();
     const text = 'title';
@@ -127,7 +128,7 @@ describe('vl-textarea', async () => {
     await assert.eventually.include(textarea.getValue(), `<h6>${text}</h6>`);
   });
 
-  it('Als gebruiker kan ik een quote tekst toevoegen', async () => {
+  it('als gebruiker kan ik een quote tekst toevoegen', async () => {
     const textarea = await vlTextareaPage.getTextareaRich();
     await textarea.clear();
     const text = 'quote';
@@ -139,7 +140,7 @@ describe('vl-textarea', async () => {
     await assert.eventually.include(textarea.getValue(), `${text}`);
   });
 
-  it('Als gebruiker kan ik een horizontale lijn tag toevoegen', async () => {
+  it('als gebruiker kan ik een horizontale lijn tag toevoegen', async () => {
     const textarea = await vlTextareaPage.getTextareaRich();
     await textarea.clear();
     const text = 'text';
@@ -149,7 +150,7 @@ describe('vl-textarea', async () => {
     await assert.eventually.include(textarea.getValue(), `<hr />`);
   });
 
-  it('Als gebruiker kan ik een genummerde lijst toevoegen', async () => {
+  it('als gebruiker kan ik een genummerde lijst toevoegen', async () => {
     const textarea = await vlTextareaPage.getTextareaRich();
     await textarea.clear();
     const text = 'item';
@@ -159,7 +160,7 @@ describe('vl-textarea', async () => {
     await assert.eventually.include(textarea.getValue(), `<ol>\n<li>${text}</li>\n</ol>`);
   });
 
-  it('Als gebruiker kan ik een lijst toevoegen', async () => {
+  it('als gebruiker kan ik een lijst toevoegen', async () => {
     const textarea = await vlTextareaPage.getTextareaRich();
     await textarea.clear();
     const text = 'item';
@@ -169,7 +170,7 @@ describe('vl-textarea', async () => {
     await assert.eventually.include(textarea.getValue(), `<ul>\n<li>${text}</li>\n</ul>`);
   });
 
-  it('Als gebruiker kan ik een link toevoegen', async () => {
+  it('als gebruiker kan ik een link toevoegen', async () => {
     const textarea = await vlTextareaPage.getTextareaRich();
     await textarea.clear();
     await textarea.addLink();
@@ -186,7 +187,7 @@ describe('vl-textarea', async () => {
     await assert.eventually.include(textarea.getValue(), `<a target="_blank" href="${link}" rel="noopener">${text}</a>`);
   });
 
-  it('Als gebruiker krijg ik een foutmelding te zien als ik geen geldige tekst of link opgeef bij het toevoegen van een link', async () => {
+  it('als gebruiker krijg ik een foutmelding te zien als ik geen geldige tekst of link opgeef bij het toevoegen van een link', async () => {
     const textarea = await vlTextareaPage.getTextareaRich();
     await textarea.clear();
     await textarea.addLink();
@@ -202,7 +203,7 @@ describe('vl-textarea', async () => {
     await modal.cancel();
   });
 
-  it('Als gebruiker kan ik de link modal annuleren en zullen bij het opnieuw openen van de link modal de input velden leeg zijn', async () => {
+  it('als gebruiker kan ik de link modal annuleren en zullen bij het opnieuw openen van de link modal de input velden leeg zijn', async () => {
     const textarea = await vlTextareaPage.getTextareaRich();
     await textarea.clear();
     await assert.eventually.isEmpty(textarea.getValue());
@@ -217,7 +218,7 @@ describe('vl-textarea', async () => {
     await modal.cancel();
   });
 
-  it('Als gebruiker maak ik een nieuw paragraph element bij elke enter', async () => {
+  it('als gebruiker maak ik een nieuw paragraph element bij elke enter', async () => {
     const textarea = await vlTextareaPage.getTextareaRich();
     await textarea.clear();
     const text = 'text';
@@ -225,7 +226,7 @@ describe('vl-textarea', async () => {
     await assert.eventually.include(textarea.getValue(), `<p>text</p>\n<p>text</p>`);
   });
 
-  it('Als gebruiker kan ik tekst kopiëren met stijl', async () => {
+  it('als gebruiker kan ik tekst kopiëren met stijl', async () => {
     if (Config.browserName != 'chrome') {
       const textarea = await vlTextareaPage.getTextareaRich();
       await textarea.clear();
@@ -239,14 +240,14 @@ describe('vl-textarea', async () => {
     }
   });
 
-  it('Als gebruiker kan ik tekst toevoegen aan een rich textarea in een shadow DOM', async () => {
+  it('als gebruiker kan ik tekst toevoegen aan een rich textarea in een shadow DOM', async () => {
     const textarea = await vlTextareaPage.getTextareaRichShadowDOM();
     const text = 'text';
     await textarea.sendKeys(text);
     await assert.eventually.include(textarea.getValue(), `${text}`);
   });
 
-  it('Als gebruiker kan ik een tekst selecteren en vervolgens een link toevoegen zonder de tekst opnieuw te moeten invullen', async () => {
+  it('als gebruiker kan ik een tekst selecteren en vervolgens een link toevoegen zonder de tekst opnieuw te moeten invullen', async () => {
     const textarea = await vlTextareaPage.getTextareaRichLink();
     await textarea.clear();
     const text = 'this is a link!';
@@ -264,7 +265,7 @@ describe('vl-textarea', async () => {
     await assert.eventually.include(textarea.getValue(), `<a target="_blank" href="${link}" rel="noopener">${text}</a>`);
   });
 
-  it('Als gebruiker kan ik een tekst met speciale characters selecteren en vervolgens een link toevoegen zonder de tekst opnieuw te moeten invullen', async () => {
+  it('als gebruiker kan ik een tekst met speciale characters selecteren en vervolgens een link toevoegen zonder de tekst opnieuw te moeten invullen', async () => {
     const textarea = await vlTextareaPage.getTextareaRichLink();
     await textarea.clear();
     const text = 'this is a <spécîàl> link!';
@@ -282,7 +283,7 @@ describe('vl-textarea', async () => {
     await assert.eventually.include(textarea.getValue(), `<a target="_blank" href="${link}" rel="noopener">this is a &lt;sp&eacute;c&icirc;&agrave;l&gt; link!</a>`);
   });
 
-  it('Als gebruiker kan ik een link bewerken', async () => {
+  it('als gebruiker kan ik een link bewerken', async () => {
     const textarea = await vlTextareaPage.getTextareaRichLink();
     await textarea.selectValue();
     await textarea.addLink();
